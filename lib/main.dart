ca80cf42f637e30912a707b5c3c65e354e8bec9d
tkdals802 <tkdals802@gmail.com>
2023-05-03 16:59:39 +0900

    first
    first

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+/* This is free and unencumbered software released into the public domain. */
+
+import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Model Viewer")),
+        body: ModelViewer(
+          backgroundColor: const Color.fromARGB(0xFF, 0xEE, 0xEE, 0xEE),
+          src: 'assets/Defeated.glb', // a bundled asset file
+          alt: "A 3D model of an astronaut",
+          ar: true,
+          arModes: const ['scene-viewer', 'webxr', 'quick-look'],
+          autoRotate: true,
+          cameraControls: true,
+          iosSrc: 'https://modelviewer.dev/shared-assets/models/Astronaut.usdz',
+          disableZoom: true,
+        ),
+      ),
+    );
+  }
+}
